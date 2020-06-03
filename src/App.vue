0c9eca48a11340d99b1604a5d5cfbf5decb7c884
long <363630867@qq.com>
2020-06-03 21:50:55 +0800

    project init and add element ui

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
     </div>
-    <router-view/>
+
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+
+export default {
+  name: "app",
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
---
+++ src/views/hello/hello/hello.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div>hello</div>
+
+</template>
+<script>
+export default {
+name: "hello",
+components: {},
+props: {},
+  data () {
+    return {
+    };
+  },
+computed: {},
+watch: {},
+mounted() {},
+destroyed() {},
+methods: {},
+}
+</script>
+<style lang="stylus" scoped>
+</style>
